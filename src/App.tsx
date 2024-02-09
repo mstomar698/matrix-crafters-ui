0d9cb7659432af52fceab77c8ae4d845c493a501
mstomar698 <tomarm698@gmail.com>
2024-02-09 19:57:52 +0530

    Update project configuration and add Matrix Crafters to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border-red-500 border-2 border-solid min-h-screen flex justify-center items-center text-center flex-col">
+      <div>Matrix Crafters</div>
+      <div>DEMO</div>
+      <div>Open in ONDC</div>
     </div>
   );
 }
